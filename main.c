4c8625ce365909592e96712e022d2f324282e196
R. G. Esteves <rodolfo.g.esteves@intel.com>
2024-02-24 09:13:26 +0000

    Trying some system-specific includes

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
 
 int main(int argc, char* argv[])
 {
+    fork();
     printf("Hello, world!\n");
-    return EXIT_SUCCESS;
+    //return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
