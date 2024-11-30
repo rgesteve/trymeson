c85b656a0c5afa2daf99f03c25656ebb56ce579d
R. G. Esteves <rodolfo.g.esteves@intel.com>
2024-11-30 06:09:11 +0000

    Looking into meson config system

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 
 #include <sys/types.h>
 
+#include "trymeson-config.h"
+
 int main(int argc, char* argv[])
 {
     if (-1 == fork()) {
         exit("fork failed!\n");
     }
     printf("Hello, world!\n");
+    printf("Version from config is: " TRYMESON_VCS_TAG ".\n");
     //return EXIT_SUCCESS;
     exit(EXIT_SUCCESS);
 }
